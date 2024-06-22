859f6017aa272aac12dc24c0bbc6767a04f49139
Shashvina Sivalingam <shashvina.sivalingam@hotmail.com>
2024-06-22 11:29:40 +0200

    test

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,9 +7,20 @@ export default function Header() {
         <ul>
           <li>
             <img width="150px" alt="Business Trips" src="/images/logo.png" />
+            <label htmlFor="seasons"></label>{" "}
+            <select id="seasons">
+              <option value="">Jaherszeiten</option>
+              <option value="1">Winter</option>
+              <option value="2">Frühling</option>
+              <option value="3">Sommer</option>
+              <option value="4">Herbst</option>
+            </select>
+
           </li>
         </ul>
       </nav>
+      
+      
     </header>
   );
 }
