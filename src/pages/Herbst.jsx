a5d99b114b557431302764bd7b01d6d797f2a6f6
Shashvina Sivalingam <shashvina.sivalingam@hotmail.com>
2024-06-26 23:00:35 +0200

    Denn Text im Herbst richtig eingefügt

--- src/pages/Herbst.jsx
+++ src/pages/Herbst.jsx
@@ -6,60 +6,47 @@ import TripList from "./TripList";
 const trips = [
   {
     id: 1,
-    title: "BT01",
-    description: "San Francisco World Trade Center on new Server/IOT/Client",
-    category: "Ausflug",
+    title: "Meeting",
+    description: "Meeting in the Office",
+    category: "Meeting",
     price: 100,
     startTrip: [2021, 2, 13, 0, 0],
     endTrip: [2021, 2, 15, 16, 56],
     meetings: [
-      {
-        id: 1,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-      {
-        id: 2,
-        title: "Zero Conference",
-        description: "Workshop Zero on One Conference",
-      },
     ],
   },
   {
     id: 2,
-    title: "BT02",
-    description: "Santa Clara Halley on new Server/IOT/Client",
-    category: "Meeting",
+    title: "Work",
+    description: "Working in the Office",
+    category: "Work",
     price: 200,
     startTrip: [2021, 6, 23, 9, 0],
     endTrip: [2021, 6, 27, 16, 56],
     meetings: [
-      {
-        id: 3,
-        title: "One Conference",
-        description: "HandsOn on One Conference",
-      },
-      {
-        id: 4,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
+      
     ],
   },
   {
     id: 3,
-    title: "BT03",
-    description: "San Cose City Halley on Docker/IOT/Client",
-    category: "Ausflug",
+    title: "Buissnes Trip",
+    description: "Autumn Buissnes Trip",
+    category: "Buissnes Trip",
     price: 150,
     startTrip: [2021, 12, 13, 9, 0],
     endTrip: [2021, 12, 15, 16, 56],
     meetings: [
-      {
-        id: 5,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Work",
+    description: "Working in the Office",
+    category: "Work",
+    price: 150,
+    startTrip: [2021, 12, 13, 9, 0],
+    endTrip: [2021, 12, 15, 16, 56],
+    meetings: [
     ],
   },
 ];
